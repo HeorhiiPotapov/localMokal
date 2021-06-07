8c3ebe4d6f620f7cc884b90ccbe1b601b0a3b7b5
whoami911329 <bayjel13@gmail.com>
2021-06-07 10:14:25 +0300

    menu for mobile resolution

--- products/forms.py
+++ products/forms.py
@@ -4,6 +4,7 @@ from .models import Product
 
 class ProductForm(forms.ModelForm):
     class Meta:
+
         model = Product
         exclude = ('is_active', 'owner', 'timestamp',
                    'slug', 'expiry_date', 'discount')
